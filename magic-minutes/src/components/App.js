fc2d8005fa6781138dc457612a9f3dbaa8263af2
Olivia Clough <olivia_clough@ucsb.edu>
2019-04-23 13:30:53 -0700

    its making me do this

--- magic-minutes/src/components/App.js
+++ magic-minutes/src/components/App.js
@@ -1,15 +1,44 @@
 //App.js
 
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
-function App(){
-	return(
-		<div>
-			<h1>Starter Code</h1>
-		</div>
-
-	)
+function Index() {
+  return <h2>Home</h2>;
 }
 
-export default App
+function Today() {
+  return <h2>Today</h2>;
+}
+
+function Plan() {
+  return <h2>Plan</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/today/">Today</Link>
+            </li>
+            <li>
+              <Link to="/plan/">Plan</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={Today} />
+        <Route path="/users/" component={Plan} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
