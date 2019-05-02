3a95618c8a31c1d5da44a4312fed6a7536e62561
Abby Wysopal <abbywysopal@ucsb.edu>
2019-05-01 19:53:35 -0700

    not sure what i changed

--- magic-minutes/src/components/App.js
+++ magic-minutes/src/components/App.js
@@ -1,58 +1,16 @@
 //App.js
 
-<<<<<<< HEAD
-import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-=======
 import React from 'react';
 import AppRouter from "./AppRouter"
->>>>>>> ed52c87049be3572da312cdcc791b566d437e4db
 
-function Index() {
-  return <h2>Home</h2>;
-}
-
-function Today() {
-  return <h2>Today</h2>;
-}
-
-function Plan() {
-  return <h2>Plan</h2>;
-}
-
-<<<<<<< HEAD
-function AppRouter() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/today/">Today</Link>
-            </li>
-            <li>
-              <Link to="/plan/">Plan</Link>
-            </li>
-          </ul>
-        </nav>
-=======
 function App(){
 	return(
 		<div>
 			<h1>Magic Minutes!</h1>
 			<AppRouter />
 		</div>
->>>>>>> ed52c87049be3572da312cdcc791b566d437e4db
-
-        <Route path="/" exact component={Index} />
-        <Route path="/about/" component={Today} />
-        <Route path="/users/" component={Plan} />
-      </div>
-    </Router>
   );
 }
 
-export default AppRouter;
+export default App;
